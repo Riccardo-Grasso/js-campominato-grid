ab1d3d30def7c7d9c3c6c733870036e5faa5b5aa
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-19 18:03:50 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
+//prendo gli elementi dal mio codice html
 const difficoltaSelezionata = document.getElementById("select-difficolta");
 const btnPlay = document.getElementById("button-play");
 const containerGriglia = document.getElementById("container-griglia");
 
 
-
+//Cosa succede quando clicco sul pulsante?
 btnPlay.addEventListener("click", function () {
-
+    //prendo la difficoltà dal form
     const difficolta = difficoltaSelezionata.value;
+
+    //calcolo il numero di celel totali con una funzione, in base alla difficoltà selezionata
     const celleTotali = functionNumeroCelle(difficolta);
 
     console.log(`L'utente ha impostato la difficoltà: ${difficolta}`);
     console.log(`Numero celle per la partita: ${celleTotali}`);
 
+    //tramite funzione, creo le celle in relazione al loro numero
     funzioneGeneraGriglia(celleTotali);
 });
 
@@ -20,8 +24,10 @@ btnPlay.addEventListener("click", function () {
 //creo funzione che mi dia il numero di celle in base alla difficoltà selezionata
 function functionNumeroCelle(difficolta) {
 
+    //Inizializzo la variabile, rendendola vuota
     let nCelle;
 
+    //opzioni di scelta con relativo numero di celle
     if (difficolta == "facile") {
         nCelle = 100;
     } else if (difficolta == "media") {
@@ -31,20 +37,23 @@ function functionNumeroCelle(difficolta) {
     }
 
     return nCelle;
-
 }
 
 //creo una funzione che, dato il numero totale di celle, le disponga nella griglia
-
 function funzioneGeneraGriglia(celleTotali) {
 
+    //svuoto la griglia ad ogni click
     containerGriglia.innerHTML = "";
 
+    //calcolo il numero di celle per ogni riga
     const cellePerRiga = Math.sqrt(celleTotali);
     console.log(`Dimensione griglia: ${cellePerRiga}x${cellePerRiga}`);
 
+    //calcolo le dimensioni di ogni cella in base al numero di celle totali
     const dimCella = 100 / cellePerRiga;
 
+
+    //un ciclo for che mi crea i div contenitori delle celle, aggiunge le classi che mi servono  e inserisce il contenuto
     for (let i = 0; i < celleTotali; i++) {
 
         const cella = document.createElement("div");
@@ -52,6 +61,8 @@ function funzioneGeneraGriglia(celleTotali) {
         cella.style.width = dimCella + "%";
         cella.style.height = dimCella + "%";
         cella.textContent = (i + 1);
+
+        //uso una nuova funzione per marcare i box che clicco
         cella.addEventListener("click", functionCellaSelezionata);
 
         containerGriglia.append(cella);
